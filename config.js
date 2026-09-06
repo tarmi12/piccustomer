01f13850dd8bee8df9a1ed678c49d1c38ae89263
tarmi12 <tarmi12t@gmail.com>
2026-09-06 21:46:09 +0700

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// config.js
-// แก้ค่าเดียวตรงนี้หลัง deploy GAS เป็น Web App แล้ว (ได้ URL ลงท้ายด้วย /exec)
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbxXtnOH5dzGwiNaRQHIxEWA1ihXd5fuKHQoFf3lGs3WWJpCwQasWij10bvUpSQ_rkhk/exec';
