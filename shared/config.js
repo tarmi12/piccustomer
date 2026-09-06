34e270a2d218076e77dff127f9440be4d64b0be8
tarmi12 <tarmi12t@gmail.com>
2026-09-06 22:29:34 +0700

    Create config.js with GAS URL
    Add configuration file for Google Apps Script URL.

---
+++ shared/config.js
@@ -0,0 +1,3 @@
+// config.js
+// แก้ค่าเดียวตรงนี้หลัง deploy GAS เป็น Web App แล้ว (ได้ URL ลงท้ายด้วย /exec)
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbxXtnOH5dzGwiNaRQHIxEWA1ihXd5fuKHQoFf3lGs3WWJpCwQasWij10bvUpSQ_rkhk/exec';
